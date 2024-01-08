2e49f1007b3bdd436f293b25aaa81a3e868e87c5
liuling <liuling005@chinasofti.com>
2024-01-08 17:25:26 +0800

    init logic

---
+++ imooc-mybatis-plus-advanced-logic/src/main/java/com/llmcu/imooc/mybatisplus/advanced/logic/Application.java
@@ -0,0 +1,13 @@
+package com.llmcu.imooc.mybatisplus.advanced.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("com.llmcu.imooc.mybatisplus.basic.environment.mapper")
+public class
+Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ imooc-mybatis-plus-advanced-logic/src/test/java/com/llmcu/imooc/mybatisplus/advanced/logic/mapper/SysUserMapperTest.java
@@ -0,0 +1,28 @@
+package com.llmcu.imooc.mybatisplus.advanced.logic.mapper;
+
+import com.llmcu.imooc.mybatisplus.advanced.logic.entity.SysUser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class SysUserMapperTest {
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+    @Test
+    @Rollback
+    public void select() {
+        List<SysUser> userList = sysUserMapper.selectList(null);
+        Assertions.assertEquals(5, userList.size());
+        SysUser user = new SysUser().setName("liuling").setAge(1);
+        System.out.println(user);
+        userList.forEach(System.out::println);
+    }
+}
